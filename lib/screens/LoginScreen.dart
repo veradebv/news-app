de32f1ef5be274e1dfce8769334928064dc4f771
veradebv <veradebora@outlook.co.id>
2025-06-07 17:02:19 +0700

    Added Home Screen and Login screen.

---
+++ lib/screens/LoginScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/widgets/LoginView.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const LoginView();
+  }
+}
